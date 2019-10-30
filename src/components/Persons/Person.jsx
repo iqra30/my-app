89a5efc123860aca3747541b2e2435b32236f618
iqra30 <abbasmeesam40@gmail.com>
2019-10-30 22:45:46 +0500

    apply css

--- src/components/Persons/Person.jsx
+++ src/components/Persons/Person.jsx
@@ -3,19 +3,19 @@ import './Person.css';
 import PersonEdit from './PersonEdit';
 import PersonInfo from './PersonInfo';
 import PropType from 'prop-types';
-import {Button,Card} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
-export default function Person(props){
+export default function Person(props) {
 
-    return(
-        <div>
-           <Card style={{ width: '18rem' }}>
-               {props.person.isEditMode ? <PersonEdit person={props.person}/> : <PersonInfo person={props.person}/>}
-           </Card>
+    return (
+        <div >
+            <Card className="Person" style={{ width: '18rem' }}>
+                {props.person.isEditMode ? <PersonEdit person={props.person} /> : <PersonInfo person={props.person} />}
+            </Card>
         </div>
     )
 }
 
 Person.PropType = {
-    person : PropType.object
+    person: PropType.object
 }
--- src/components/Persons/Persons.jsx
+++ src/components/Persons/Persons.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 import './Persons.css';
 import Person from './Person';
 
-export default function Persons(){
+export default function Persons() {
     const personsData = [
         {
-         "name" : "Alaina Sarfaraz",
-         "age" : 20,
-         "description" : "Freelancer",
-         "isEditMode" : true
+            "name": "Alaina Sarfaraz",
+            "age": 20,
+            "description": "Freelancer",
+            "isEditMode": false
         },
         {
-        "name" : "Saqib Siddiqui",
-         "age" : 22,
-         "description" : "Mobile app designer",
-         "isEditMode" : false
+            "name": "Saqib Siddiqui",
+            "age": 22,
+            "description": "Mobile app designer",
+            "isEditMode": false
 
         },
         {
-         "name" : "Mahanoor Faraz",
-         "age" : 23,
-         "description" : "Graphic designer",
-         "isEditMode" : false
+            "name": "Mahanoor Faraz",
+            "age": 23,
+            "description": "Graphic designer",
+            "isEditMode": false
 
         }
     ]
-    return(
-        <div>
-          
-          <h1>Persons </h1>
-          {
-              personsData.map(person => <Person person={person}/>)
-          }
+    return (
+        <div className="Persons">
+
+            <h1>Persons </h1>
+            {
+                personsData.map(person => <Person person={person} />)
+            }
         </div>
     )
 }
