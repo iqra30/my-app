3003634ef74f653fa37d921ac13f4008a456ea8e
iqra30 <abbasmeesam40@gmail.com>
2019-10-30 23:58:44 +0500

    Update Persons.jsx

--- src/components/Persons/Persons.jsx
+++ src/components/Persons/Persons.jsx
@@ -1,42 +1,64 @@
-import React from 'react';
+
 import './Persons.css';
 import Person from './Person';
+import React, { Component } from 'react';
 
-export default function Persons(props) {
-    const personsData = [
-        {
-            "name": "Alaina Sarfaraz",
-            "age": 20,
-            "description": "Freelancer",
-            "isEditMode": false
-        },
-        {
-            "name": "Saqib Siddiqui",
-            "age": 22,
-            "description": "Mobile app designer",
-            "isEditMode": false
+class Persons extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            personsData: [
+                {
+                    "name": "Alaina Sarfaraz",
+                    "age": 20,
+                    "description": "Freelancer",
+                    "isEditMode": false
+                },
+                {
+                    "name": "Saqib Siddiqui",
+                    "age": 22,
+                    "description": "Mobile app designer",
+                    "isEditMode": false
 
-        },
-        {
-            "name": "Mahanoor Faraz",
-            "age": 23,
-            "description": "Graphic designer",
-            "isEditMode": false
+                },
+                {
+                    "name": "Mahanoor Faraz",
+                    "age": 23,
+                    "description": "Graphic designer",
+                    "isEditMode": false
 
+                }
+            ]
         }
-    ];
-    const enableEdit = () => {
-        alert('hello duniya');
     }
-    return (
-        <div className="Persons">
+    enableEdit = (p) => {
+        this.setState({
+            personsData: this.state.personsData.map(
+                pInner => {
+                    if(pInner.name === p.name){
+                        pInner.isEditMode =true;
+                    }
+                    return pInner;
+                }
+            )
+        })
+        p.isEditMode = true;
+    }
+    render() {
+        return (
+            <div className="Persons">
 
-            <h1>Persons </h1>
-            {
-                personsData.map(person => <Person
-                    person={person}
-                    editButtonMode={enableEdit} />)
-            }
-        </div>
-    )
+                <h1>Persons </h1>
+                {
+                    this.state.personsData.map(person => <Person
+                        person={person}
+                        editButtonMode={
+                            () => this.enableEdit(person)} />)
+                }
+            </div>
+        )
+    }
 }
+
+export default Persons;
+
